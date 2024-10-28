b482f44b9dace9485e45531f1a4c10a004745e3d
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-28 21:34:51 +0100

    Řešení příkladu validace

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log(validator)
 console.log(validator.isEmail("juracakova.vera@seznam.cz"))
 console.log(validator.isCreditCard("4125010001000208", "['mastercard','visa]"))
 
@@ -8,7 +7,8 @@ const message = document.querySelector("#msg")
 if(validator.isEmail(email)) {
     message.textContent = "Email je v pořádku.";
     message.classList.add("msg--valid")
+    console.log("ok");
 } else {
-    message.textContent = "Nesprávny email.";
+    message.textContent = "Neplatný email.";
     message.classList.add("msg--invalid")
 }
